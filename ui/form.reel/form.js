f80720198a274c3b1358ad04fb15026bd14b5f4d
lojosmit <noreply>
2015-09-30 15:18:21 +0000

    Add component form

---
+++ ui/form.reel/form.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/form.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Form
+ * @extends Component
+ */
+exports.Form = Component.specialize(/** @lends Form# */ {
+    constructor: {
+        value: function Form() {
+            this.super();
+        }
+    }
+});
